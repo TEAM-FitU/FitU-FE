59df6b8cf27fb9faf14162fdd231c503d3502237
jiruen <kjr2699@naver.com>
2025-05-16 02:45:53 +0900

    feat(#5): Header 컴포넌트 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import logo from '/logo.png';
 
-function Header() {
+function Header({ fixed }) {
     return (
-        <header className="h-12 shadow flex items-center justify-between px-6">
-            <img src={logo} alt="FitU 로고" className="h-12"/>
+        <header
+            className={
+                `bg-white h-12 shadow flex items-center justify-between px-6 
+                ${fixed ? 'fixed top-0 left-0 w-full z-30' : ''}`
+            }
+        >
+            <img src={logo} alt="FitU 로고" className="h-12" />
 
             <div className="font-semibold text-sm">
                 마이페이지
             </div>
         </header>
-
     );
 }
 

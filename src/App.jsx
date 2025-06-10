512142a2b0300b65f04c2216a533b14f12b3f932
ottozip <ottozipx@gmail.com>
2025-06-10 16:41:34 +0900

    feat(#3): RecommendationResultPage 구현  및 유효성 검사 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from "./components/Header.jsx";
+import SetSituationPage from './pages/SetSituationPage/SetSituationPage.jsx';
+import RecommendationResultPage from './pages/RecommendationResultPage/RecommendationResultPage.jsx';
 
 function App() {
     return (
         <>
-            <Header/>
+            {/* <Header/> */}
+            {/* <SetSituationPage/> */}
+            <RecommendationResultPage/>
         </>
     )
 }
